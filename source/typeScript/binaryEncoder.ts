8bc67e034aedd3cf6c0b791e7da31250994ecd78
narumincho <narumincho.starfy@gmail.com>
2020-02-25 21:01:07 +0900

    uInt32Code local to export function

--- source/typeScript/binaryEncoder.ts
+++ source/typeScript/binaryEncoder.ts
@@ -1,3 +1,4 @@
+import * as generator from "js-ts-code-generator";
 import { expr, typeExpr } from "js-ts-code-generator";
 
 /**
@@ -16,71 +17,76 @@ const mathObject = expr.globalVariable("Math");
    ========================================
 */
 
-const uInt32Name = ["decodeUInt32"];
-export const uInt32Var = expr.localVariable(uInt32Name);
+export const uInt32Name = "decodeUInt32";
 
 /**
  * numberの32bit符号なし整数をUnsignedLeb128で表現されたバイナリに変換するコード
  */
-export const uInt32Code = expr.functionWithReturnValueVariableDefinition(
+export const uInt32Code: [string, generator.ExportFunction] = [
   uInt32Name,
-  [{ name: ["num"], typeExpr: typeExpr.typeNumber }],
-  readonlyArrayNumber,
-  [
-    expr.set(
-      expr.localVariable(["num"]),
-      null,
-      expr.callMethod(mathObject, "floor", [
-        expr.callMethod(mathObject, "max", [
-          expr.numberLiteral(0),
-          expr.callMethod(mathObject, "min", [
-            expr.localVariable(["num"]),
-            expr.numberLiteral(2 ** 32 - 1)
-          ])
-        ])
-      ])
-    ),
-    expr.variableDefinition(
-      ["numberArray"],
-      readonlyArrayNumber,
-      expr.arrayLiteral([])
-    ),
-    expr.whileTrue([
-      expr.variableDefinition(
-        ["b"],
-        typeExpr.typeNumber,
-        expr.bitwiseAnd(
-          expr.localVariable(["num"]),
-          expr.numberLiteral(0b1111111)
-        )
-      ),
+  {
+    document:
+      "numberの32bit符号なし整数をUnsignedLeb128で表現されたバイナリに変換するコード",
+    parameterList: [
+      { name: "num", typeExpr: typeExpr.typeNumber, document: "" }
+    ],
+    returnType: readonlyArrayNumber,
+    statementList: [
       expr.set(
         expr.localVariable(["num"]),
         null,
-        expr.unsignedRightShift(
-          expr.localVariable(["num"]),
-          expr.numberLiteral(7)
-        )
-      ),
-      expr.ifStatement(
-        expr.equal(expr.localVariable(["num"]), expr.numberLiteral(0)),
-        [
-          expr.evaluateExpr(
-            expr.callMethod(expr.localVariable(["numberArray"]), "push", [
-              expr.localVariable(["b"])
+        expr.callMethod(mathObject, "floor", [
+          expr.callMethod(mathObject, "max", [
+            expr.numberLiteral(0),
+            expr.callMethod(mathObject, "min", [
+              expr.localVariable(["num"]),
+              expr.numberLiteral(2 ** 32 - 1)
             ])
-          ),
-          expr.returnStatement(expr.localVariable(["numberArray"]))
-        ]
-      ),
-      expr.evaluateExpr(
-        expr.callMethod(expr.localVariable(["numberArray"]), "push", [
-          expr.bitwiseOr(
-            expr.localVariable(["b"]),
-            expr.numberLiteral(0b10000000)
-          )
+          ])
         ])
-      )
-    ])
-  ]
-);
+      ),
+      expr.variableDefinition(
+        ["numberArray"],
+        readonlyArrayNumber,
+        expr.arrayLiteral([])
+      ),
+      expr.whileTrue([
+        expr.variableDefinition(
+          ["b"],
+          typeExpr.typeNumber,
+          expr.bitwiseAnd(
+            expr.localVariable(["num"]),
+            expr.numberLiteral(0b1111111)
+          )
+        ),
+        expr.set(
+          expr.localVariable(["num"]),
+          null,
+          expr.unsignedRightShift(
+            expr.localVariable(["num"]),
+            expr.numberLiteral(7)
+          )
+        ),
+        expr.ifStatement(
+          expr.equal(expr.localVariable(["num"]), expr.numberLiteral(0)),
+          [
+            expr.evaluateExpr(
+              expr.callMethod(expr.localVariable(["numberArray"]), "push", [
+                expr.localVariable(["b"])
+              ])
+            ),
+            expr.returnStatement(expr.localVariable(["numberArray"]))
+          ]
+        ),
+        expr.evaluateExpr(
+          expr.callMethod(expr.localVariable(["numberArray"]), "push", [
+            expr.bitwiseOr(
+              expr.localVariable(["b"]),
+              expr.numberLiteral(0b10000000)
+            )
+          ])
+        )
+      ])
+    ]
+  }
+];
