4c5be1a58de72955e4caa1039691f4a066738cb0
narumincho <narumincho.starfy@gmail.com>
2020-03-11 15:29:25 +0900

    add test code

--- testOut/main.ts
+++ testOut/main.ts
@@ -1,14 +1,15 @@
 import * as out from "./out";
+import * as cliColor from "cli-color";
 
-const objectEqual = (a: unknown, b: unknown): boolean => {
+const objectEqual = <T>(a: T, b: T): boolean => {
   if (a === b) {
     return true;
   }
   if (isPrimitive(a) || isPrimitive(b)) {
     return false;
   }
-  for (const [k, v] of Object.entries(a)) {
-    if (!objectEqual(b[k], v)) {
+  for (const key of new Set([...Object.keys(a), ...Object.keys(b)])) {
+    if (!objectEqual(a[key], b[key])) {
       return false;
     }
   }
@@ -28,17 +29,38 @@ const isPrimitive = (value: unknown): boolean => {
   );
 };
 
-const maybeStringJsValue = out.maybeJust("それな");
-console.group("maybe string");
-console.log("js value", maybeStringJsValue);
-const maybeStringBinary = out.encodeMaybe(out.encodeString)(maybeStringJsValue);
-console.log("binary", maybeStringBinary);
-const maybeStringDecodedJsValue = out.decodeMaybe(out.decodeString)(
-  0,
-  new Uint8Array(maybeStringBinary)
+const test = <T>(
+  title: string,
+  jsValue: T,
+  encodeFunction: (a: T) => ReadonlyArray<number>,
+  decodeFunction: (a: number, b: Uint8Array) => { result: T; nextIndex: number }
+) => {
+  console.group(title);
+  console.log("js value        :", jsValue);
+  const binary = encodeFunction(jsValue);
+  console.log("binary          :", binary);
+  const decodedJsValue = decodeFunction(0, new Uint8Array(binary)).result;
+  console.log("decoded js value:", decodedJsValue);
+  if (objectEqual(jsValue, decodedJsValue)) {
+    console.log(cliColor.red("failure!!"));
+  } else {
+    console.log(cliColor.green("ok"));
+  }
+  console.log();
+  console.groupEnd();
+};
+
+test("string", "sample text", out.encodeString, out.decodeString);
+test(
+  "maybe string",
+  out.maybeJust("それな"),
+  out.encodeMaybe(out.encodeString),
+  out.decodeMaybe(out.decodeString)
 );
-console.log("decoded js value", maybeStringDecodedJsValue);
-console.log(
-  "equal",
-  objectEqual(maybeStringJsValue, maybeStringDecodedJsValue)
+test(
+  "list number",
+  [1, 43, 6423, 334, 663, 74],
+  out.encodeList(out.encodeInt32),
+  out.decodeList(out.decodeInt)
 );
+test("custom", out.typeList(out.typeInt), out.encodeType, out.decodeType);
