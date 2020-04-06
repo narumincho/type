68167830df6a5507086bcb0e40cb22bcfbca96b8
narumincho <narumincho.starfy@gmail.com>
2020-04-06 18:51:21 +0900

    update js-ts-code-generator  0.2.6

--- testOut/out.ts
+++ testOut/out.ts
@@ -28,7 +28,16 @@ export type Type =
 /**
  * 正常値と異常値
  */
-export type ResultType = { ok: Type; error: Type };
+export type ResultType = {
+  /**
+   * 正常値
+   */
+  ok: Type;
+  /**
+   * 異常値
+   */
+  error: Type;
+};
 
 /**
  * 英語,日本語,エスペラント語などの言語
@@ -39,10 +48,25 @@ export type Language = "Japanese" | "English" | "Esperanto";
  * デバッグモードかどうか,言語とページの場所. URLとして表現されるデータ. Googleなどの検索エンジンの都合( https://support.google.com/webmasters/answer/182192?hl=ja )で,URLにページの言語のを入れて,言語ごとに別のURLである必要がある. デバッグ時のホスト名は http://[::1] になる
  */
 export type UrlData = {
+  /**
+   * クライアントモード
+   */
   clientMode: ClientMode;
+  /**
+   * 場所
+   */
   location: Location;
+  /**
+   * 言語
+   */
   language: Language;
+  /**
+   * アクセストークン. ログインした後のリダイレクト先としてサーバーから渡される
+   */
   accessToken: Maybe<AccessToken>;
+  /**
+   * 予約語をこっそり入れてみる
+   */
   if: boolean;
 };
 
@@ -62,7 +86,20 @@ export type Location =
 /**
  * プロジェクト
  */
-export type Project = { name: string; icon: FileHash; image: FileHash };
+export type Project = {
+  /**
+   * プロジェクト名
+   */
+  name: string;
+  /**
+   * プロジェクトのアイコン画像
+   */
+  icon: FileHash;
+  /**
+   * プロジェクトのカバー画像
+   */
+  image: FileHash;
+};
 
 export type AccessToken = string & { _accessToken: never };
 
@@ -380,8 +417,8 @@ export const decodeInt32 = (
   index: number,
   binary: Uint8Array
 ): { result: number; nextIndex: number } => {
-  let result: number = 0;
-  let offset: number = 0;
+  let result = 0;
+  let offset = 0;
   while (true) {
     const byte: number = binary[index + offset];
     result |= (byte & 127) << (offset * 7);
